6d372f7bfa18f816af6f944792ab985ea1f7d1f2
jjkim90 <jaejun8613@gmail.com>
2023-03-09 20:36:12 +0900

    Create P2588.java

---
+++ BOJ/bronze/P2588.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class P2588 {
+    public static void main(String[] agrs){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a * (b % 10));
+        System.out.println(a * ((b % 100) / 10));
+        System.out.println(a * (b / 100));
+        System.out.println(a * b);
+    }
+}
