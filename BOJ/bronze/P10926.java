2d8aaea3071476aa21d357316f3401e14c4e41a2
jjkim90 <jaejun8613@gmail.com>
2023-03-09 20:36:18 +0900

    Create P10926.java

---
+++ BOJ/bronze/P10926.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class P10926{
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        
+        String wowString = "??!";
+        
+        String input = sc.next();
+        System.out.println(input + wowString);
+    }
+}
