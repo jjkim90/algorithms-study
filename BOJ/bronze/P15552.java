cd08e40fe5ec25c906a83c135d8bd6679f9f3f06
jjkim90 <jaejun8613@gmail.com>
2023-03-14 23:01:32 +0900

    Update P15552.java

--- BOJ/bronze/P15552.java
+++ BOJ/bronze/P15552.java
@@ -1,20 +1,24 @@
 import java.io.*;
 import java.util.StringTokenizer;
+
 public class P15552 {
  
 	public static void main(String[] args) throws IOException {
  
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int c = Integer.parseInt(br.readLine());
+		
+		for(int i = 0;i<c;i++){
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-		int total = Integer.parseInt(br.readLine());
-
-		for(int i = 0; i<total;i++){
-			String s = br.readLine();
-			int a = Integer.parseInt(s.split(" ")[0]);
-			int b = Integer.parseInt(s.split(" ")[1]);
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
 			bw.write(a+b+"\n");
 		}
+
 		bw.flush();
 	}
 }
