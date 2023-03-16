5f5ff0a34cb20ff486facebddeea9f11dd0b66b4
jjkim90 <jaejun8613@gmail.com>
2023-03-16 23:36:17 +0900

    Create P10951.java

---
+++ BOJ/bronze/P10951.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.StringTokenizer;
+public class P10951 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String line;
+
+        while((line = br.readLine()) != null){
+            if(line.length() == 0) break;
+
+            StringTokenizer st = new StringTokenizer(line," ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            bw.write(a+b+"\n");
+        }
+        bw.flush();
+        
+    }
+}
