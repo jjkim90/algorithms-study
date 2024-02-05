debd4241c0e861a225551a12b025d4dd6b33f9d7
jjkim90 <jaejun8613@gmail.com>
2024-02-05 18:18:49 +0900

    Initialize

--- BOJ/bronze/P2743.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-
-public class P2743 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String str = reader.readLine();
-        System.out.println(str.length());
-    }   
-}
